4bdc1fb9583ca875d7facd23caaa7266023e3790
Rane <github@blasph.me>
2019-07-31 19:14:32 -0700

    added post / for stretch (previously created seed files, which is also stretch)

--- data/db.js
+++ data/db.js
@@ -4,6 +4,7 @@ const config = require('../knexfile').development;
 const db = knex(config);
 
 const getProjects = () => db('projects');
+
 const getProject = async (id) => {
     const project = await db('projects').where({ id });
     const actions = await db('actions').where({project_id: id });
@@ -11,8 +12,6 @@ const getProject = async (id) => {
     const newProject = Object.assign({}, project[0]);
     newProject.completed = newProject.completed === 0 ? false:true;
     newProject.actions = actions;
-
-
 
     if (newProject) return newProject;
     else return null;
